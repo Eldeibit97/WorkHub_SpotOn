8d8f1503189bb6c5b1477faa187ce8cee7b15b04
HighTec5589Pro <calkestisxdq@gmail.com>
2026-04-14 18:53:08 -0600

    Pagina y Frontend del Login creado

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -3,17 +3,19 @@ import { Routes, Route } from 'react-router-dom'
 import CrearReservacion from './components/creacion/CrearReservacion'
 import Confirmacion from './components/creacion/Confirmacion'
 import LandingPage from './views/LandingPage'
+import SignInPage from './views/SignInPage'
 import AppShell from './AppShell'
 import ManageReservationsPage from './Pages/ManageReservationsPage'
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/*" element={<AppShell />} />
       <Route path="/" element={<LandingPage />} />
-      <Route path='/reservar' element={<CrearReservacion />} />
-      <Route path='/confirmacion' element={<Confirmacion />} />
-      <Route path='/cancelar' element={<ManageReservationsPage/>} />
+      <Route path="/login" element={<SignInPage />} />
+      <Route path="/reservar" element={<CrearReservacion />} />
+      <Route path="/confirmacion" element={<Confirmacion />} />
+      <Route path="/cancelar" element={<ManageReservationsPage />} />
+      <Route path="/*" element={<AppShell />} />
     </Routes>
   )
 }
