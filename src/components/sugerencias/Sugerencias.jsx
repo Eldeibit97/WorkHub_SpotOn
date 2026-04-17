227222953cc0ddfab7829684baaf2846c75b71f5
Eldeibit97 <david.mtz.rscn@gmail.com>
2026-04-17 17:17:40 -0600

    feat: Pagina de sugerencias en progreso

---
+++ src/components/sugerencias/Sugerencias.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import '../../styles/sugerencias.css'
+
+const Sugerencias = () => {
+  const getCurrentDate = () => {
+    const today = new Date();
+    const days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+    const months = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'];
+
+    const dayName = days[today.getDay()];
+    const monthName = months[today.getMonth()];
+    const day = today.getDate();
+    const year = today.getFullYear();
+
+    return `${dayName}, ${monthName} ${String(day).padStart(2, '0')} of ${year}`;
+  };
+
+  return (
+    <>
+      <div className='today-info-container'>
+        <p className='greeting'>Buenos dias ...</p>
+        <p className='date'>{getCurrentDate()}</p>
+      </div>
+      <div className='suggestions-container'>
+        <div className="info-box"></div>
+        <div className="info-box">
+          <p className='info-box-title'>Sobre tu ruta...</p>
+          <p className='info-box-text'>ETA </p>
+        </div>
+        <div className="info-box">
+          <p className='info-box-title'>Sugerencias</p>
+        </div>
+      </div>
+      <button className='reserve-button'> Reservar lugar → </button>
+    </>
+  )
+}
+
+export default Sugerencias
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Sugerencias from './components/sugerencias/Sugerencias.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Sugerencias />
   </StrictMode>,
 )
