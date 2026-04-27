74641cfcc76b520c264569075411cfb730c3f358
Eldeibit97 <david.mtz.rscn@gmail.com>
2026-04-27 15:53:24 -0600

    feat: Pantalla de error para creacion de reservas

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import CrearReservacion from './views/CrearReservacion'
 import Confirmacion from './components/creacion/Confirmacion'
+import Sugerencias from './views/Sugerencias'
+import Error from './components/creacion/Error'
 import LandingPage from './views/LandingPage'
 import SignInPage from './views/SignInPage'
 import ManageReservationsPage from './views/ManageReservationsPage'
@@ -11,8 +13,10 @@ const AppRoutes = () => {
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/login" element={<SignInPage />} />
+      <Route path='/sugerencias' element={<Sugerencias />}/>
       <Route path="/reservar" element={<CrearReservacion />} />
       <Route path="/confirmacion" element={<Confirmacion />} />
+      <Route path='/error' element = {<Error />} />
       <Route path="/cancelar" element={<ManageReservationsPage />} />
       <Route path="/*" element={<LandingPage />} />
     </Routes>
