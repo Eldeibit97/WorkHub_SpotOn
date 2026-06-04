0d5542c9ede54579d47107db316995a12a28953a
Eldeibit97 <david.mtz.rscn@gmail.com>
2026-06-03 22:17:16 -0600

    fix: Date strip con constante faltante MONTS_ES

--- src/pages/CrearReservacion/components/DateStrip.jsx
+++ src/pages/CrearReservacion/components/DateStrip.jsx
@@ -3,7 +3,7 @@ import { formatDateWeekdayDayMonthEs, isSameLocalDay } from '../../../lib/dateFo
 import './DateStrip.css'
 
 const DAYS_ES = ['dom', 'lun', 'mar', 'mié', 'jue', 'vie', 'sáb']
-
+const MONTHS_ES = {1: 'enero', 2: 'febrero', 3: 'marzo', 4: 'abril', 5: 'mayo', 6: 'junio', 7: 'julio', 8: 'agosto', 9: 'septiembre', 10: 'octubre', 11: 'noviembre', 12: 'diciembre'}
 /** Cabeceras lun → dom (misma convención que la tira semanal). */
 const WEEKDAY_LABELS_MON = [...DAYS_ES.slice(1), DAYS_ES[0]]
 
